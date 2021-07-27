cd32b62ae10a01ea684eb6c0660f9d231a2609bb
MrWD10 <waleedakhtar484@gmail.com>
2021-07-27 09:58:07 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_practice/counter.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+// ignore: use_key_in_widget_constructors
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          value: Counter(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+// ignore: use_key_in_widget_constructors
+class MyHomePage extends StatelessWidget {
+  void _incrementCounter(BuildContext context) {
+    Provider.of<Counter>(context, listen: false).incrementCounter();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var counter = Provider.of<Counter>(context).getCounter;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Provider practice"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _incrementCounter(context),
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
